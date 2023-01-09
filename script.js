fbbd92be1c5ac67925a825897713780943ec2cfa
Nichole Lin <77648096+Nichole1412@users.noreply.github.com>
2023-01-09 02:18:33 +0000

    Added all the links and resume, about and contact page

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+$(".about").hide();
+$(".contact").hide();
+$(".Resume").hide();
 $(".Protfoliobtn").click(function(){
   $(".ddcontent").slideToggle("1s");
 });
+
+$(".Aboutbtn").click(function(){
+  $(".main").hide();
+  $(".about").show();
+  $(".contact").hide();
+  $(".Resume").hide();
+});
+
+$(".Contactbtn").click(function(){
+  $(".main").hide();
+  $(".about").hide();
+  $(".contact").show();
+  $(".Resume").hide();
+})
+$(".Resumebtn").click(function(){
+  $(".main").hide();
+  $(".about").hide();
+  $(".contact").hide();
+  $(".Resume").show();
+})
